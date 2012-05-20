c72b369dd9db2e4ee1b10909eb95e304a0534848
Nikolay Kuchumov <kuchumovn@gmail.com>
2012-05-20 18:26:34 +0400

    cleaning up

--- sources/Main.scala
+++ sources/Main.scala
@@ -3,45 +3,42 @@ import org.mortbay.jetty.servlet.{Context, ServletHolder}
 
 import tools._
 
-object Main
+object Main extends App
 {
-	def main(args: Array[String]) 
+	// создаём Jetty
+	val server = new Server(System.getProperty("port").toInt)
+	val root = new Context(server, "/", Context.SESSIONS)
+	
+	// в каком пакете искать наши Rest веб-сервисы
+	val package_name : String = System.getProperty("package")
+	
+	// каждый класс ScalatraServlet из этого пакета (и подпакетов) "замапить" на свой путь.
+	// путь определяется методом "path" этого класса.
+	for (handler <- PackageScanner.getClasses(package_name) if classOf[org.scalatra.ScalatraServlet].isAssignableFrom(handler))
 	{
-		// создаём Jetty
-		val server = new Server(Some[Int](System.getProperty("port").toInt).getOrElse(8080))
-		val root = new Context(server, "/", Context.SESSIONS)
+		// создаём сервлет из этого класса
+		val servlet = handler.newInstance().asInstanceOf[javax.servlet.Servlet]
 		
-		// в каком пакете искать наши Rest веб-сервисы
-		val package_name : String = System.getProperty("package")
-		
-		// каждый класс ScalatraServlet из этого пакета (и подпакетов) "замапить" на свой путь.
-		// путь определяется методом "path" этого класса.
-		for (handler <- PackageScanner.getClasses(package_name) if classOf[org.scalatra.ScalatraServlet].isAssignableFrom(handler))
+		// на какой путь "замапим" сервлет
+		try
 		{
-			// создаём сервлет из этого класса
-			val servlet = handler.newInstance().asInstanceOf[javax.servlet.Servlet]
+			// получаем это из метода path()
+			val path = handler.getMethod("path").invoke(servlet).toString
 			
-			// на какой путь "замапим" сервлет
-			try
-			{
-				// получаем это из метода path()
-				val path = handler.getMethod("path").invoke(servlet).toString
-				
-				// "мапим" сервлет на этот путь в Jetty
-				root.addServlet(new ServletHolder(servlet), path + "/*")
-				
-				System.out.println(handler.getPackage().getName() + "." + handler.getName() + " is mapped to " + path)
-			}
-			catch 
-			{
-				// не написан метод path() у сервлета
-				case ioe: NoSuchMethodException => 
-					throw new RuntimeException("Method path() not found in class " + handler.getPackage().getName() + "." + handler.getName())
-			}
+			// "мапим" сервлет на этот путь в Jetty
+			root.addServlet(new ServletHolder(servlet), path + "/*")
+			
+			println(handler.getPackage().getName() + "." + handler.getName() + " is mapped to " + path)
 		}
-		
-		// запускаем Jetty
-		server.start()
-		server.join()
+		catch 
+		{
+			// не написан метод path() у сервлета
+			case error: NoSuchMethodException => 
+				throw new RuntimeException("Method path() not found in class " + handler.getPackage().getName() + "." + handler.getName())
+		}
 	}
+	
+	// запускаем Jetty
+	server.start()
+	server.join()
 }
